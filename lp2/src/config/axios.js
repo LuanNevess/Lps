af30450364668271b1f60379aa295367b6c83ea6
LuanNevess <luanzito809@gmail.com>
2025-10-30 00:32:52 -0300

    Update axios.js

--- lp2/src/config/axios.js
+++ lp2/src/config/axios.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://my-json-server.typicode.com/MauroLucasO/jsonfake-1';
+export const BASE_URL = 'https://my-json-server.typicode.com/LuanNevess/jsonfake';
 //'https://my-json-server.typicode.com/LuanNevess/jsonfake';
 
 
